5b48b7cb8c9eac0c75c3f7dcf5d9d8373bbe0e4c
mehdi-jawad98 <mehdijroud98@gmail.com>
2021-11-22 04:13:11 +0100

    Add JSON and the theme files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  primaryColor: "#18d26e", //Color for box, buttons...
+  bodyColor: "#f8f8f8", //Color for box, buttons...
+  prColor: "#222", //Color for Paragraphs and sections background
+  mainFont: "'Roboto', sans-serif",
+  titleFont: "'Ubuntu', sans-serif",
+};
